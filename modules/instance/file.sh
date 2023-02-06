597987ca521d75f1a9ab63f0e79de8f866f714b1
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2023-02-06 18:54:16 +0530

    Update file.sh

--- modules/instance/file.sh
+++ modules/instance/file.sh
@@ -1,14 +1,3 @@
-#! /bin/bash
-
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/keypair | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/natgw | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/pbroutetable  | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/pbsg | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/private_subnet | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/public_subnet | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/pvtroutetable  | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/pvtsg   | terraform init
-cd /home/dheerendra/dheerendra/dheeru/os_tf/modules/vpc | terraform init
 
 
 
